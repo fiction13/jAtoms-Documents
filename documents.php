ff2bf0655aeec6f9b223905182fa554c1ecb7ee1
fiction13 <fiction13@yandex.ru>
2022-06-29 15:15:21 +0300

    Change version 1.0.0

--- documents.php
+++ documents.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * @package    jAtomS - Documents Plugin
- * @version    __DEPLOY_VERSION__
+ * @version    1.0.0
  * @author     Atom-S - atom-s.com
  * @copyright  Copyright (c) 2017 - 2021 Atom-S LLC. All rights reserved.
  * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
@@ -19,7 +19,7 @@ class plgJAtomSDocuments extends CMSPlugin
 	 *
 	 * @var  boolean
 	 *
-	 * @since   __DEPLOY_VERSION__
+	 * @since   1.0.0
 	 */
 	protected $autoloadLanguage = true;
 }
